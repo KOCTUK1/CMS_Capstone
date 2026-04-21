a95fd44ca1d22c97b8f4daf649eedea55a657202
KOCTUK1 <koctuk@rollins.edu>
2026-04-20 21:27:57 -0400

    Add redirect from root to admin page

--- website/website/urls.py
+++ website/website/urls.py
@@ -14,10 +14,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import include, path
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
+    path("", RedirectView.as_view(url="/admin/")),
     path("predictor/", include("predictor.urls")),
     path("admin/", admin.site.urls),
 ]
